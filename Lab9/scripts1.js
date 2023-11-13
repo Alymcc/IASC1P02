1214c8b34154915f1d593118b9b4dc81ee1596fc
Alymcc <144562869+Alymcc@users.noreply.github.com>
2023-11-13 01:04:15 -0500

    update

--- Lab9/scripts1.js
+++ Lab9/scripts1.js
@@ -1,47 +1,26 @@
-// Get the current date and time
-
-   var date = new Date();
+ var date = new Date();
 
 
+   var start = date;
 
-   // Get the start and end times of the alert
+   var stop = new Date();
 
-   var startTime = date;
-
-   var endTime = new Date();
-
-
-
-   // Create a function to format the time as mm:ss
 
    function formatTime(time) {
 
-     var milliseconds = Math.floor(startTime.getMilliseconds());
+     var milliseconds = Math.floor(start.getMilliseconds());
 
-     return milliseconds;
+     return milliseconds;}
 
-   }
-
-
-
-   // Create a function to display the alert
 
    function stopTime() {
 
-     // Get the current time in milliseconds
 
      var currentTime = date.getTime();
 
 
-
-     // Calculate the difference between the start and end times in milliseconds
-
-     var difference = endTime.getTime() - startTime.getTime();
+     var difference = stop.getTime() - start.getTime();
 
 
 
-     // Display the alert with the formatted time
-
-     alert("You have been on this page for : " + formatTime(difference));
-
-   }
+     alert("You have been on this page for : " + formatTime(difference));}
