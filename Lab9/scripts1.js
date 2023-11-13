18bd7aec2e0172f935ea3a4ab6e9f7cb2046d199
Alymcc <144562869+Alymcc@users.noreply.github.com>
2023-11-13 00:20:04 -0500

    exam1

--- Lab9/scripts1.js
+++ Lab9/scripts1.js
@@ -1,38 +1,47 @@
-var exactDate = new Date();
+// Get the current date and time
 
-var startTime =exactDate.getTime()
-
-
-function StopTime()
-{console.log("user pressed the Stop time button");
-document.getElementById("DATE").innerHTML = year+"/"+month+"/"+date;}
+   var date = new Date();
 
 
 
+   // Get the start and end times of the alert
 
-console.log(exactDate);
-console.log("Pull pices from exactDate");
+   var startTime = date;
 
-
-var month =exactDate.getMonth() +1;// to avoid 0 based indexing error
-var year =exactDate.getFullYear()
-var date =exactDate.getDate()
+   var endTime = new Date();
 
 
 
+   // Create a function to format the time as mm:ss
 
-console.log("Month:" +month)
-console.log("Year:" +year)
-console.log("Date:" +date)
+   function formatTime(time) {
 
+     var milliseconds = Math.floor(startTime.getMilliseconds());
 
+     return milliseconds;
 
-
-
-exactDate = new Date();
-var endTime = exactDate.getTime();
-console.log("page log took" +(endTime-startTime)+"ms")
+   }
 
 
-var myalert = alert("endTime");
-console.log(myalert);
+
+   // Create a function to display the alert
+
+   function stopTime() {
+
+     // Get the current time in milliseconds
+
+     var currentTime = date.getTime();
+
+
+
+     // Calculate the difference between the start and end times in milliseconds
+
+     var difference = endTime.getTime() - startTime.getTime();
+
+
+
+     // Display the alert with the formatted time
+
+     alert("You have been on this page for : " + formatTime(difference));
+
+   }
