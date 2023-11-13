f854d1540a1f681ee72398814eddd0deec12cfbf
Alymcc <144562869+Alymcc@users.noreply.github.com>
2023-11-13 00:55:00 -0500

    exam3

--- Lab9/scripts3.js
+++ Lab9/scripts3.js
@@ -1,3 +1,25 @@
-console.log("Page load start")
+// Define a function to add two numbers
 
-function nothing(){console.log("user pressed the nothing button");}
+  function addNumbers() {
+
+    // Get the values of the input boxes
+
+    var num1 = document.getElementById("num1").value;
+
+    var num2 = document.getElementById("num2").value;
+
+
+
+    // Convert the values to numbers
+
+    num1 = Number(num1);
+
+    num2 = Number(num2);
+
+
+
+    // Calculate the sum of the numbers
+
+    var sum = num1 + num2;
+
+    output.innerHTML = sum;}
