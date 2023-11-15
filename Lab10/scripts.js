3bf6f41ade87c2b61c8c9c57d922d6c761abb33c
Alymcc <144562869+Alymcc@users.noreply.github.com>
2023-11-14 22:50:20 -0500

    Lab10

---
+++ Lab10/scripts.js
@@ -0,0 +1,31 @@
+function addNumbers() {
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  num1 = Number(num1);
+  num2 = Number(num2);
+  var sum = num1 + num2;
+  output.innerHTML = sum;}
+
+function subNumbers() {
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  num1 = Number(num1);
+  num2 = Number(num2);
+  var sum = num1 - num2;
+  output.innerHTML = sum; }
+
+function multiNumbers() {
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  num1 = Number(num1);
+  num2 = Number(num2);
+  var sum = num1 * num2;
+  output.innerHTML = sum;}
+
+function diviNumbers() {
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  num1 = Number(num1);
+  num2 = Number(num2);
+  var sum = num1 / num2;
+  output.innerHTML = sum;}
